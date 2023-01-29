e7684b2663a7a63e3ee66d5661934c0acc27e743
Luke Rollans <luke@rollans.sh>
2023-01-29 14:34:55 +0800

    Sync to S3 and invalidate Cloudfront distro (untested)

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import assert from "assert"
 
 import * as core from "@actions/core"
 
+// @ts-expect-error no types here just yet
+import { sync } from "s3-sync-client"
+
+import {
+  CloudFrontClient,
+  CreateInvalidationCommand,
+  CreateInvalidationCommandInput,
+} from "@aws-sdk/client-cloudfront"
+
 async function run(): Promise<void> {
   try {
     // Retrieve all required inputs
@@ -33,6 +42,22 @@ async function run(): Promise<void> {
       process.env.AWS_SECRET_ACCESS_KEY !== undefined,
       "`AWS_SECRET_ACCESS_KEY` is not set in the environment. Has a previous action setup AWS credentials?"
     )
+
+    // Sync `from` input path up to `s3-path` using `s3-client-sync` package
+    await sync(s3Path, sourcePath, { del: true })
+
+    // Invalidate the Cloudfront distribution so updated files will be
+    // served from the S3 bucket
+    const cf = new CloudFrontClient({})
+    const result = await cf.send(
+      new CreateInvalidationCommand({
+        DistributionId: cfDistroId,
+        InvalidationBatch: undefined,
+      })
+    )
+
+    // Set the invalidation's ID as an output and finish up!
+    core.setOutput("cloudfront-invalidation-id", result.Invalidation?.Id)
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
